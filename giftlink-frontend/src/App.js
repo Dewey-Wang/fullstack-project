72625fa4bba7904d43320b5b355cd6be2ec51e56
Ding Yang Wang <82537687+Dewey-Wang@users.noreply.github.com>
2025-01-28 13:13:45 +0100

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
+import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -16,9 +17,10 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/app" element={<MainPage />} />
-          <Route path="/app/search" element={<SearchPage/>} />
           <Route path="/app/login" element={<LoginPage/>} />
           <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/search" element={<SearchPage/>} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
           <Route path="/app/profile" element={<Profile/>} />
         </Routes>
         </>
